bf823810cffeb9617da7f9d656e4990d3051414a
Li Boxuan <li-boxuan@github.com>
2017-10-26 10:15:47 +0800

    modify write benchmark

--- write_benchmark.c
+++ write_benchmark.c
@@ -7,6 +7,7 @@
 
 #define BUF_SIZE 5000
 #define BUF_LEN 4096
+#define TOT_LEN 1024 * 1024 * 100
 
 static inline double millitime(){
     struct timeval now;
@@ -29,28 +30,40 @@ int main(int argc, char* argv[])
     int i;
     for (i = 0; i < len; i++)
         buf[i] = '+';
+    long cnt = 0;
+    double min_latency = 10000;
+    double tot_latency = 0;
+    double max_latency = 0;
     double stime = millitime();
-    long tot = 0;
-    while (tot < 1024 * 1024 * 1024)
+    while (cnt < TOT_LEN)
     {
+    	double stime = millitime();
         ret = write(fd, buf, len);
         if (ret != len)
         {
             perror("write");
             abort();
         }
-        tot += ret;
+        cnt += ret;
         ret = fsync(fd);
         if (ret != 0)
         {
             perror("fsync");
             abort();
         }
+	double latency = millitime() - stime;
+	if (latency < min_latency)
+	    min_latency = latency;
+	if (latency > max_latency)
+	    max_latency = latency;
+	tot_latency += latency;
     }
     double time_cost = millitime() - stime;
-    double tput = tot / 1024 / 1024 / time_cost; 
-    printf("\rThroughput (MB/s): %.6f", tput);
-    fflush(stdout);
-
+    double tput = cnt / 1024 / 1024 / time_cost; 
+    printf("Throughput (MB/s): %.6f\n", tput);
+    printf("min_latency = %f\n", min_latency * 1000000);
+    printf("max_latency = %f\n", max_latency * 1000000);
+    printf("tot_latency = %f\n", tot_latency * 1000000);
+    printf("avg_latency = %f\n", tot_latency / cnt * BUF_LEN * 1000000);
     return 0;
 }
